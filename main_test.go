0b385b85bb64592318c55b35d423294b22017a79
hermannhahn <hermann.h.hahn@gmail.com>
2022-07-19 14:01:03 -0300

    TestRemoveMockProductFromDatabase added

--- main_test.go
+++ main_test.go
@@ -16,8 +16,9 @@ var ID int
 
 // SetupTestRoutes is a function that returns a router with all routes
 func SetupTestRoutes(route string) *gin.Engine {
-	database.Connect()
 	r := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
+	database.Connect()
 	if route == "products" {
 		r.GET("/products", controllers.ShowProducts)
 	}
@@ -49,7 +50,7 @@ func TestStatusOK(t *testing.T) {
 
 func TestCreateMockProduct(t *testing.T) {
 	SetupTestRoutes("create")
-	product := models.Product{Name: "Testing CreateProduct", Description: "Test Description", Price: 1.1, Quantity: 1, Image: "http://test.com/test.jpg", Active: true}
+	product := models.Product{Name: "TestingCreateProduct", Description: "Test Description", Price: 1.1, Quantity: 1, Image: "http://test.com/test.jpg", Active: true}
 	database.DB.Create(&product)
 	ID = 0
 	assert.NotEqual(t, 0, product.ID, "Product not created. ID should not be 0")
@@ -60,9 +61,19 @@ func TestUpdateMockProduct(t *testing.T) {
 	SetupTestRoutes("update")
 	var product models.Product
 	database.DB.First(&product, ID)
-	product.Name = "Testing UpdateProduct"
+	product.Name = "TestingUpdateProduct"
 	database.DB.Save(&product)
-	assert.Equal(t, "Testing UpdateProduct", product.Name, "Product not updated")
+	assert.Equal(t, "TestingUpdateProduct", product.Name, "Product not updated")
+}
+
+func TestSearchMockProduct(t *testing.T) {
+	r := SetupTestRoutes("search")
+	var product models.Product
+	database.DB.First(&product, ID)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/products/s/TestingUpdateProduct", nil)
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code, "Status code should be 200")
 }
 
 func TestDeleteMockProduct(t *testing.T) {
@@ -72,4 +83,13 @@ func TestDeleteMockProduct(t *testing.T) {
 	database.DB.First(&product, ID)
 	database.DB.Delete(&product)
 	assert.NotEqual(t, notDeleted, product.DeletedAt, "Product not deleted")
+}
+
+func TestRemoveMockProductFromDatabase(t *testing.T) {
+	SetupTestRoutes("delete")
+	var product models.Product
+	database.DB.Unscoped().First(&product, ID)
+	notDeleted := product.ID
+	database.DB.Unscoped().Delete(&product)
+	assert.Equal(t, notDeleted, product.ID, "Product not removed from database")
 }
